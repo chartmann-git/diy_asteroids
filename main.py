28076a16f0f1e5a3945b65167aeff28e44c03020
chartmann-git <charleshhartmann@protonmail.com>
2025-06-12 15:53:39 -0400

    Finished Ch2, working player sprite

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -23,13 +26,16 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill("black")
-        player.draw(screen)
-        pygame.display.flip()
         
-        dt = clock.tick(60) / 1000
+        screen.fill("black")
+        
 
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
 
 if __name__ == "__main__":
     main()
